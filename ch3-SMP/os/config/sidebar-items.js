00b1eea70d5fc0db8b1c8e888188dcaa47cb83d1
msyzzz <msyzzz@users.noreply.github.com>
2022-04-30 08:35:13 +0000

    deploy: 69a0146d2150ce32f77f1dd867faab5884506686

---
+++ ch3-SMP/os/config/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["APP_BASE_ADDRESS",""],["APP_SIZE_LIMIT",""],["CPU_NUM",""],["FIRST_CPU",""],["KERNEL_HEAP_SIZE",""],["KERNEL_STACK_SIZE",""],["MAX_APP_NUM",""],["USER_STACK_SIZE",""]]});
---
+++ ch3-SMP/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["os"] = {"name":"","dirs":[{"name":"boards","files":["qemu.rs"]},{"name":"syscall","files":["fs.rs","mod.rs","process.rs"]},{"name":"task","files":["context.rs","mod.rs","switch.rs","task.rs"]},{"name":"trap","files":["context.rs","mod.rs"]}],"files":["config.rs","console.rs","harts.rs","heap_allocator.rs","lang_items.rs","loader.rs","main.rs","sbi.rs","timer.rs"]};
+createSourceSidebar();
